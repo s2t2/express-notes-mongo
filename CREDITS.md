d96dde2775574c41501646bd7a4524aefb2176d3
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-03-27 23:43:34 -0600

    deleting all existing notes before seeding a new one

--- CREDITS.md
+++ CREDITS.md
@@ -8,3 +8,8 @@
  + https://gist.github.com/fwielstra/1025038
  + http://stackoverflow.com/questions/12669615/add-created-at-and-updated-at-fields-to-mongoose-schemas
  + http://mongoosejs.com/docs/guide.html#timestamps
+ + http://mongoosejs.com/docs/api.html#model_Model.remove
+
+## Debugging
+
+ + https://nodejs.org/api/debugger.html -- place `debugger;` in the code and run the script with `node debug myscript.js` (jk this doesn't work as expected https://github.com/nodejs/node/issues/5930)
--- db/seeds.js
+++ db/seeds.js
@@ -3,17 +3,29 @@ var Note = require("../app/models/note");
 
 mongoose.connect('mongodb://localhost/notes');
 
-var todo = new Note({
-  title: 'Todo Today',
-  description: 'Something I need to do today.'
-});
+// get a collection of all existing notes
 
-todo.save(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('dooo itt!', todo.title);
-  }
+Note.find(function (err, notes) {
+  if (err) return console.error(err);
+  console.log("FOUND", notes.length, "NOTES TO BE DELETED")
 
-  mongoose.disconnect(); // close the connection, or else it will keep running, which is appropriate for when the web server runs, but not for a script like this.
+  // delete all items in the collection
+
+  Note.remove(notes, function (err) {
+    if (err) return console.error(err);
+    console.log("DELETED")
+
+    // add new note(s) to the collection
+
+    var todo = new Note({title: 'Todo Today', description: 'Something I need to do today.'});
+    todo.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('dooo itt!', todo.title);
+      }
+
+      mongoose.disconnect(); // close the connection, or else it will keep running, which is appropriate for when the web server runs, but not for a script like this.
+    });
+  });
 });
